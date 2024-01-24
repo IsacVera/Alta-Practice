3ab6dbcfedaafbfc94d29e9e289ec66e6f4a4961
IsacVera <isaacvera37@icloud.com>
2024-01-24 14:02:03 -0800

    updates resetFile function to copy source files and paste to practice folder

--- FileEditor.go
+++ FileEditor.go
@@ -6,26 +6,51 @@ import (
     "log"
 )
 
-func createFiles(fileName string) {
+func resetFile(practiceFile *os.File, algo string, state string) {
+    var fileSource string
+    if state == "complete" {
+        fileSource = fmt.Sprintf("./DataAlgoSource/%sComplete.txt", algo)
+    } else {
+        fileSource = fmt.Sprintf("./DataAlgoSource/%sIncomplete.txt", algo)
+    }
+
+    byteAlgoCode, sourceErr := os.ReadFile(fileSource)
+    if sourceErr != nil {
+        log.Fatalln("Failed: Reading Source File")
+    }
+
+    algoCode := string(byteAlgoCode)
+
+    _, err := practiceFile.WriteString(algoCode)
+    if err != nil {
+        log.Fatalln("Failed: Reseting File")
+    }
+}
+
+func createFile(fileName string, algo string) {
     createdFile, err := os.Create(fileName)
 
     if err != nil {
         log.Fatalln("Failed: Creating File")
     }
 
+    resetFile(createdFile, algo, "reset")
+    
+
     defer createdFile.Close()
 }
 
-func checkFiles(algorithms [2]string) string {
+func checkFiles(algorithms [3]string) string {
     var algo string
     for i:=0; i < len(algorithms); i++ {
         algo = algorithms[i] 
+
         filePath := fmt.Sprintf("./DataAlgoPractice/%s.go", algo)
         _, err := os.Stat(filePath) 
 
         if err != nil {
             fileName := filePath[2:]
-            createFiles(fileName)
+            createFile(fileName, algo)
         }
     }
 
